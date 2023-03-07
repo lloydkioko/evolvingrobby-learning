14c6c1f91af9c8c6bceb54ecb0b8f4d131568491
lloyd kioko <ch.abene254@gmail.com>
2023-03-07 10:58:02 +0300

    #1 Setup Project

---
+++ src/main/java/ke/co/technovation/evolving/robby/EvolvingRobbyApplication.java
@@ -0,0 +1,4 @@
+package ke.co.technovation.evolving.robby;
+
+public class EvolvingRobbyApplication {
+}
---
+++ src/test/java/ke/co/technovation/evolving/robby/EvolvingRobbyApplicationTests.java
@@ -0,0 +1,16 @@
+package ke.co.technovation.evolving.robby;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;;
+
+
+
+public class EvolvingRobbyApplicationTests {
+    @Test
+    void contextLoads() {
+        assertThat(true).isFalse();
+    }
+
+
+}
